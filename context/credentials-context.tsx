994ea6a37422c634957551cc3912a5805267bf76
crypaus <124097061+crypaus@users.noreply.github.com>
2023-12-07 14:26:32 -0500

    Update credentials-context.tsx

--- context/credentials-context.tsx
+++ context/credentials-context.tsx
@@ -3,11 +3,11 @@ import cookies from "js-cookie"
 
 const credentials_cookie_key = "credentials"
 const initialCredentials = {
-  openaiApiKey: "",
-  pineconeEnvironment: "",
-  pineconeIndex: "",
-  pineconeApiKey: "",
-  githubPersonalToken: "",
+  openaiApiKey: "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",
+  pineconeEnvironment: "eu-west4-gcp",
+  pineconeIndex: "test",
+  pineconeApiKey: "b9262921-b168-478f-875d-a925ed377957",
+  githubPersonalToken: "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",
 }
 
 const CredentailsCookieContext = createContext({
